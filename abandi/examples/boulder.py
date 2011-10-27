7b0d2ab99054c38bef85366c9afcb39593b2880e
ponty <titi@pince.(none)>
2011-10-27 10:24:11 +0200

    new example

---
+++ abandi/examples/boulder.py
@@ -0,0 +1,21 @@
+'''
+1. list boulder games 
+2. start selected game
+'''
+
+from abandi.search import search_games
+from abandi.run import run_game
+from psidialogs import choice
+import unicodedata
+from entrypoint2 import entrypoint
+def norm(s):
+    return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
+
+@entrypoint
+def main():
+    games = search_games(where='name like "%boulder%"')
+    dic = dict([ (norm(x.name), x) for x in games])
+    name = choice(sorted(dic.keys()))
+    if name:
+        game = dic[name]
+        run_game(game.source, game.id, auto_install=1)
